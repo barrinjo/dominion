6f7a3944fc1e7fe01cca6de4cbee48c403620d46
muchgangster <jbarringer1999@gmail.com>
2019-05-27 15:04:20 -0700

    actions working.  setting up kingdoms next

--- phase.hpp
+++ phase.hpp
@@ -16,20 +16,35 @@ bool handCheck(std::vector<card *> deck, type t) {
     return false;
 }
 
+inline bool isInteger(const std::string & s)
+{
+   if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
+
+   char * p;
+   strtol(s.c_str(), &p, 10);
+
+   return (*p == 0);
+}
+
 void actionPhase() {
     std::cout << "action phase: " << std::endl;
     player *p = playerList[turn];
     std::vector<card *> hand = p->getHand();
     bool exit = !handCheck(hand, ACTION);
     std::string input;
+    p->printHand();
     while(!exit) {
-        p->printHand();
         std::cout << "play an action card" << std::endl;
         std::cin >> input;
-        if(input == "quit")
-            exit = true;
-        else
+        if(isInteger(input)) {
             p->playCard(std::stoi(input));
+            p->printHand();
+        } else if (input == "end") {
+            exit = true;
+        } else {
+            std::cout << "input not recognized" << std::endl;
+        }
+        exit = handCheck(hand, ACTION);
     }
 }
 
@@ -48,14 +63,14 @@ void buyPhase() {
 }
 
 void cleanPhase() {
-    std::cout << "test" << std::endl;
     player *p = playerList[turn];
     p->clearAll();
     p->draw(5);
     p->setGold(0);
     p->setActions(1);
     p->setBuys(1);
-    int temp;
+    std::string temp;
+    std::cout << "Enter \'0\' to Continue" << std::endl;
     std::cin >> temp;
 }
 
